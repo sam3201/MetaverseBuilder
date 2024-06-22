444b77f5b883c95af35a2c2212e109de666ee709
Sam3201 <2023samd@gmail.com>
2024-06-22 14:53:42 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ read -p "sim or game " file
 
 case "$file" in "game") gcc -o game game.c utils/environment.c  -lpthread && ./game && rm game && exit 1;; esac 
 case "$file" in "sim") gcc -o sim sim.c utils/environment.c -lpthread && ./sim && rm sim && exit 1 ;; esac 
-case "$file" in "server") gcc -o server server.c -pthread && ./server 127.0.0.1 42069 && rm server && exit 1 ;; esac
-case "$file" in "client") gcc -o client client.c -pthread && ./client 127.0.0.1 42069 && rm client && exit 1 ;; esac
+case "$file" in "server") gcc -o server utils/server.c -pthread && ./server 127.0.0.1 42069 && rm server && exit 1 ;; esac
+case "$file" in "client") gcc -o client utils/client.c -pthread && ./client 127.0.0.1 42069 && rm client && exit 1 ;; esac
 case "$file" in *) echo "Invalid Option: $file" && exit 1 ;; esac
