8874a41900cde0b651cbd33d320829be130aac4b
Sam3201 <2023samd@gmail.com>
2024-07-15 22:19:50 -0400

    Update PredPreySim.c

--- VERSION/0.0.1A/src/PredPreySim.c
+++ VERSION/0.0.1A/src/PredPreySim.c
@@ -26,8 +26,8 @@ typedef struct {
     NN_t *nn;
     Entity *entity;
     double fitness;
-    int is_predator;
-    int time_alive;
+    size_t is_predator;
+    size_t time_alive;
 } Agent;
 
 typedef struct {
@@ -38,9 +38,9 @@ typedef struct {
     Agent *predators[MAX_PREDATORS];
     Agent *preys[MAX_PREY];
     Food *foods[MAX_FOOD];
-    int numPredators;
-    int numPreys;
-    int numFoods;
+    size_t numPredators;
+    size_t numPreys;
+    size_t numFoods;
 } Simulation;
 
 double distance(Pos A, Pos B) {
@@ -58,7 +58,7 @@ void destroyFood(Food *food) {
     free(food);
 }
 
-Agent *createAgent(Canvas *canvas, const char *type, char symbol, int is_predator) {
+Agent *createAgent(Canvas *canvas, const char *type, char symbol, size_t is_predator) {
     Agent *agent = malloc(sizeof(Agent)); 
     if (!agent) {
         fprintf(stderr, "Failed to allocate memory for agent\n");
@@ -79,8 +79,21 @@ Agent *createAgent(Canvas *canvas, const char *type, char symbol, int is_predato
     agent->is_predator = is_predator;
     agent->entity->health = INITIAL_HEALTH;
     agent->time_alive = 0;
+    
+    ActivationFunction hidden_activations[20];
+    ActivationFunction hidden_derivatives[20];
+    ActivationFunction output_activations[4];
+    ActivationFunction output_derivatives[4];
+    for (size_t i = 0; i < 20; i++) {
+        hidden_activations[i] = sigmoid;
+        hidden_derivatives[i] = sigmoid_derivative;
+    }
+    for (size_t i = 0; i < 4; i++) {
+        output_activations[i] = tanh;
+        output_derivatives[i] = tanh_derivative;
+    }
 
-    agent->nn = NN_create(5, 20, 4, NULL, NULL, NULL, NULL, 0.01, 0.9);
+    agent->nn = NN_create(5, 20, 4, hidden_activations, hidden_derivatives, output_activations, output_derivatives, 0.01, 0.9);
     if (!agent->nn) {
         fprintf(stderr, "Failed to create neural network for agent\n");
         destroyAgent(agent);
@@ -109,11 +122,11 @@ Food *createFood(Canvas *canvas) {
     return food;
 }
 
-int checkAliveEntities(Simulation *simulation) {
-    for (int i = 0; i < simulation->numPredators; i++) {
+size_t checkAliveEntities(Simulation *simulation) {
+    for (size_t i = 0; i < simulation->numPredators; i++) {
         if (simulation->predators[i]->entity->health > 0) return 1;
     }
-    for (int i = 0; i < simulation->numPreys; i++) {
+    for (size_t i = 0; i < simulation->numPreys; i++) {
         if (simulation->preys[i]->entity->health > 0) return 1;
     }
     return 0;
@@ -137,8 +150,8 @@ void updateAgent(Agent *agent, Canvas *canvas, Simulation *simulation) {
 
     forward(agent->nn, inputs);
 
-    int max_index = 0;
-    for (int i = 1; i < 4; i++) {
+    size_t max_index = 0;
+    for (size_t i = 1; i < 4; i++) {
         if (agent->nn->output[i] > agent->nn->output[max_index]) {
             max_index = i;
         }
@@ -173,7 +186,7 @@ void updateAgent(Agent *agent, Canvas *canvas, Simulation *simulation) {
 
     if (!agent->is_predator && closest_food && min_food_distance < CATCH_DISTANCE) {
         agent->entity->health += PREY_GAIN;
-        for (int i = 0; i < simulation->numFoods; i++) {
+        for (size_t i = 0; i < simulation->numFoods; i++) {
             if (simulation->foods[i] == closest_food) {
                 destroyFood(closest_food);
                 simulation->foods[i] = simulation->foods[--simulation->numFoods];
@@ -184,18 +197,18 @@ void updateAgent(Agent *agent, Canvas *canvas, Simulation *simulation) {
 
     if (agent->entity->cell.c == 'X') {
         agent->fitness = calculatePredatorFitness(agent);
-        backprop(agent->nn, &agent->fitness);
+        //backprop(agent->nn, &agent->fitness);
   } 
     if (agent->entity->cell.c == 'O') {
         agent->fitness = calculatePreyFitness(agent);
-        backprop(agent->nn, &agent->fitness);
+        //backprop(agent->nn, &agent->fitness);
   } 
     
 }
 
 /*
 void mutate(NN_t *nn) {
-    for (int i = 0; i < nn->numWeights; i++) {
+    for (size_t i = 0; i < nn->numWeights; i++) {
         if ((double)rand() / RAND_MAX < MUTATION_RATE) {
             nn->weights[i] += ((double)rand() / RAND_MAX - 0.5) * 0.1;
         }
@@ -206,7 +219,7 @@ void mutate(NN_t *nn) {
 
 /*
 void crossover(NN_t *parent1, NN_t *parent2, NN_t *child) {
-    for (int i = 0; i < parent1->numWeights; i++) {
+    for (size_t i = 0; i < parent1->numWeights; i++) {
         if ((double)rand() / RAND_MAX < CROSSOVER_RATE) {
             child->weights[i] = parent1->weights[i];
         } else {
@@ -216,7 +229,7 @@ void crossover(NN_t *parent1, NN_t *parent2, NN_t *child) {
 }
 */
 
-int compareFitness(const void *a, const void *b) {
+size_t compareFitness(const void *a, const void *b) {
     Agent *agentA = *(Agent**)a;
     Agent *agentB = *(Agent**)b;
     return (agentB->fitness > agentA->fitness) - (agentB->fitness < agentA->fitness);
@@ -227,16 +240,16 @@ void evolvePopulation(Simulation *simulation) {
     qsort(simulation->predators, simulation->numPredators, sizeof(Agent*), compareFitness);
     qsort(simulation->preys, simulation->numPreys, sizeof(Agent*), compareFitness);
 
-    for (int i = MAX_PREDATORS / 2; i < MAX_PREDATORS; i++) {
-        int parent1 = rand() % (MAX_PREDATORS / 2);
-        int parent2 = rand() % (MAX_PREDATORS / 2);
+    for (size_t i = MAX_PREDATORS / 2; i < MAX_PREDATORS; i++) {
+        size_t parent1 = rand() % (MAX_PREDATORS / 2);
+        size_t parent2 = rand() % (MAX_PREDATORS / 2);
         crossover(simulation->predators[parent1]->nn, simulation->predators[parent2]->nn, simulation->predators[i]->nn);
         mutate(simulation->predators[i]->nn);
     }
 
-    for (int i = MAX_PREY / 2; i < MAX_PREY; i++) {
-        int parent1 = rand() % (MAX_PREY / 2);
-        int parent2 = rand() % (MAX_PREY / 2);
+    for (size_t i = MAX_PREY / 2; i < MAX_PREY; i++) {
+        size_t parent1 = rand() % (MAX_PREY / 2);
+        size_t parent2 = rand() % (MAX_PREY / 2);
         crossover(simulation->preys[parent1]->nn, simulation->preys[parent2]->nn, simulation->preys[i]->nn);
         mutate(simulation->preys[i]->nn);
     }
@@ -244,10 +257,10 @@ void evolvePopulation(Simulation *simulation) {
 */
 
 void updateSimulation(Simulation *simulation, Canvas *canvas) {
-    for (int i = 0; i < simulation->numPredators; i++) {
+    for (size_t i = 0; i < simulation->numPredators; i++) {
         updateAgent(simulation->predators[i], canvas, simulation);
     }
-    for (int i = 0; i < simulation->numPreys; i++) {
+    for (size_t i = 0; i < simulation->numPreys; i++) {
         updateAgent(simulation->preys[i], canvas, simulation);
     }
 
@@ -262,13 +275,13 @@ void updateSimulation(Simulation *simulation, Canvas *canvas) {
 }
 
 void destroySimulation(Simulation *simulation) {
-    for (int i = 0; i < simulation->numPredators; i++) {
+    for (size_t i = 0; i < simulation->numPredators; i++) {
         destroyAgent(simulation->predators[i]);
     }
-    for (int i = 0; i < simulation->numPreys; i++) {
+    for (size_t i = 0; i < simulation->numPreys; i++) {
         destroyAgent(simulation->preys[i]);
     }
-    for (int i = 0; i < simulation->numFoods; i++) {
+    for (size_t i = 0; i < simulation->numFoods; i++) {
         destroyFood(simulation->foods[i]);
     }
     free(simulation);
@@ -285,7 +298,7 @@ Simulation* createSimulation(Canvas *canvas) {
     simulation->numPreys = MAX_PREY;
     simulation->numFoods = MAX_FOOD / 2;  
 
-    for (int i = 0; i < MAX_PREDATORS; i++) {
+    for (size_t i = 0; i < MAX_PREDATORS; i++) {
         simulation->predators[i] = createAgent(canvas, "PREDATOR", 'X', 1);
         if (!simulation->predators[i]) {
             fprintf(stderr, "Failed to create predator\n");
@@ -293,7 +306,7 @@ Simulation* createSimulation(Canvas *canvas) {
             return NULL;
         }
     }
-    for (int i = 0; i < MAX_PREY; i++) {
+    for (size_t i = 0; i < MAX_PREY; i++) {
         simulation->preys[i] = createAgent(canvas, "PREY", 'O', 0);
         if (!simulation->preys[i]) {
             fprintf(stderr, "Failed to create prey\n");
@@ -301,7 +314,7 @@ Simulation* createSimulation(Canvas *canvas) {
             return NULL;
         }
     }
-    for (int i = 0; i < simulation->numFoods; i++) {
+    for (size_t i = 0; i < simulation->numFoods; i++) {
         simulation->foods[i] = createFood(canvas);
         if (!simulation->foods[i]) {
             fprintf(stderr, "Failed to create food\n");
@@ -314,44 +327,44 @@ Simulation* createSimulation(Canvas *canvas) {
 }
 
 void restartSimulation(Simulation *simulation, Canvas *canvas) {
-    for (int i = 0; i < simulation->numPredators; i++) {
+    for (size_t i = 0; i < simulation->numPredators; i++) {
         destroyAgent(simulation->predators[i]);
         simulation->predators[i] = createAgent(canvas, "PREDATOR", 'X', 1);
     }
     
-    for (int i = 0; i < simulation->numPreys; i++) {
+    for (size_t i = 0; i < simulation->numPreys; i++) {
         destroyAgent(simulation->preys[i]);
         simulation->preys[i] = createAgent(canvas, "PREY", 'O', 0);
     }
     
-    for (int i = 0; i < simulation->numFoods; i++) {
+    for (size_t i = 0; i < simulation->numFoods; i++) {
         destroyFood(simulation->foods[i]);
     }
     simulation->numFoods = MAX_FOOD / 2;
-    for (int i = 0; i < simulation->numFoods; i++) {
+    for (size_t i = 0; i < simulation->numFoods; i++) {
         simulation->foods[i] = createFood(canvas);
     }
 }
 
 void drawSimulation(Canvas *canvas, Simulation *simulation) {
-    for (int i = 0; i < simulation->numPredators; i++) {
+    for (size_t i = 0; i < simulation->numPredators; i++) {
         Entity *entity = simulation->predators[i]->entity;
         canvas->state.cells[entity->cell.pos.y][entity->cell.pos.x] = entity->cell.c;
         canvas->state.colors[entity->cell.pos.y][entity->cell.pos.x] = entity->color;
     }
-    for (int i = 0; i < simulation->numPreys; i++) {
+    for (size_t i = 0; i < simulation->numPreys; i++) {
         Entity *entity = simulation->preys[i]->entity;
         canvas->state.cells[entity->cell.pos.y][entity->cell.pos.x] = entity->cell.c;
         canvas->state.colors[entity->cell.pos.y][entity->cell.pos.x] = entity->color;
     }
-    for (int i = 0; i < simulation->numFoods; i++) {
+    for (size_t i = 0; i < simulation->numFoods; i++) {
         Entity *entity = simulation->foods[i]->entity;
         canvas->state.cells[entity->cell.pos.y][entity->cell.pos.x] = entity->cell.c;
         canvas->state.colors[entity->cell.pos.y][entity->cell.pos.x] = entity->color;
     }
 }
 
-int run(uint8_t frameRate, uint8_t rows, uint8_t cols) {
+size_t run(uint8_t frameRate, uint8_t rows, uint8_t cols) {
     signal(SIGINT, handleSignal);
     srand((unsigned)time(NULL));
 
